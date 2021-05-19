3af8f2684d7629c81bd5c44b0afefe29a5bc1753
LeoRoma <xiajtn@gmail.com>
2021-05-19 12:19:58 +0100

    Dark mode completed

--- src/toggle.js
+++ src/toggle.js
@@ -1,43 +1,20 @@
 const toggle = document.querySelector('.toggle');
-// const indicator = document.querySelector('.indicator');
-
-
-
-
-// Element to change once the toggle button is triggered
-
-// body => change color
 const body = document.querySelector('body');
-
-
-// navbar font color 
 const navbarItems = document.querySelector('.navbar-container');
-
-// hero name
 const heroNames = document.querySelectorAll('.hero-name-container h1');
-
-
-// hero info
 const heroInfos = document.querySelectorAll('.hero-info');
-
-
-// svg lines
-
-console.log(paths)
-
-
-// projects title
 const projectsTitles = document.querySelectorAll('.project-title');
+const footerItems = document.querySelectorAll('.footer-item');
+const textAndBorder = document.querySelector('.cd-back-cover');
+
+
+
 // #C5C8C2
-
-// footer
-
-const footerItems = document.querySelectorAll('.footer-item')
 
 // Cd backcover
 
 // border lines & text
-const textAndBorder = document.querySelector('.cd-back-cover');
+
 const cdTitleLine = document.querySelector('.cd-title');
 const cdBodyLine = document.querySelector('.cd-body');
 const cdBodyMiddleLine = document.querySelector('.body-web');
@@ -54,4 +31,57 @@ const bullseyeOuterCircle = document.querySelector('.bullseye-outer-circle');
 toggle.addEventListener('click', () => {
     toggle.classList.toggle('active');
     body.classList.toggle('active');
+    navbarItems.classList.toggle('active');
+    changeHeroNamesColour();
+    changeHeroInfosColour();
+    changeSVGLinesColour();
+    changeProjectsTitleColour();
+    changeFooterItemsColour();
+    textAndBorder.classList.toggle('active');
+    changeEllipsesColour();
+    bullseyeOuterCircle.classList.toggle('active');
+    bullseyeInnerCircle.classList.toggle('active');
+    bullseyeCenter.classList.toggle('active');
 })
+
+function changeHeroNamesColour(){
+    for(let i = 0; i < heroNames.length; i++){
+        const heroName = heroNames[i];
+        heroName.classList.toggle('active');
+    }
+}
+
+function changeHeroInfosColour(){
+    for(let i = 0; i < heroInfos.length; i++){
+        const heroInfo = heroInfos[i];
+        heroInfo.classList.toggle('active');
+    }
+}
+
+function changeSVGLinesColour(){
+    for(let i = 0; i < paths.length; i++){
+        const path = paths[i];
+        path.classList.toggle('active');
+    }
+}
+
+function changeProjectsTitleColour(){
+    for(let i = 0; i < projectsTitles.length; i++){
+        const projectsTitle = projectsTitles[i];
+        projectsTitle.classList.toggle('active');
+    }
+}
+
+function changeFooterItemsColour(){
+    for(let i = 0; i < footerItems.length; i++){
+        const footerItem = footerItems[i];
+        footerItem.classList.toggle('active');
+    }
+}
+
+function changeEllipsesColour(){
+    for(let i = 0; i < ellipses.length; i++){
+        const ellipse = ellipses[i];
+        ellipse.classList.toggle('active');
+    }
+}
