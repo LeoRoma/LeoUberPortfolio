ef1da877ed04b9019cd6a43f24cb958957719ebd
LeoRoma <xiajtn@gmail.com>
2021-05-20 21:33:48 +0100

    Hold the line wobblying on release

--- src/wobblyLine.js
+++ src/wobblyLine.js
@@ -13,30 +13,30 @@ for (let i = 0; i < paths.length; i++) {
     const svg = svgs[i];
     const path = paths[i];
 
-    path.addEventListener("mouseenter", (event) => {
-        // if (!connected && event.target === path) {
-        //     connected = true;
-        //     console.log(i) 
-        // }
+    svg.addEventListener("mousemove", (event) => {
+        if (!connected && event.target === path) {
+            connected = true;
+            console.log(i) 
+        }
 
-        // if (connected) {
-        //     mousePosition.y = event.pageY * 2 - (mousePosition.y + mousePosition.y) / 2;
-        //     update(path)
-        // }
-        console.log(i) 
-        update(path)
+        if (connected) {
+            mousePosition.y = event.pageY * 2 - (mousePosition.y + mousePosition.y) / 2;
+            update(path)
+        }
+        // console.log(i) 
+        // update(path)
     });
 
-    path.addEventListener("mouseleave", () => {
-        gsap.to(path, { ease: "elastic(3, 0.01)", attr: { d: "M0,10 Q50,10 100,10" } })
+    svg.addEventListener("mouseleave", () => {
+        gsap.to(path, { ease: "elastic(5, 0.1)", attr: { d: "M0,10 Q50,10 100,10" } })
     })
 }
 
 function update(path) {
-    let d = `M0,10 Q50,50 100,10`;
+    let d = `M0,10 Q50,20 100,10`;
 
     path.setAttribute("d", d);
-    // if (Math.abs(mousePosition.y - startY) > snapDist * 2) {      
+    // if (mousePosition.y >= 20 || mousePosition.y <= 0) {      
     //     connected = false;
     //     gsap.to(mouseCoords, { duration: 1,  y: startY });
     //     gsap.to(path, { ease: "elastic(3, 0.01)", attr: { d: "M0,10 Q50,10 100,10" } })
