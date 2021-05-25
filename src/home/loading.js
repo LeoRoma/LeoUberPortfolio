bbe2d90365d0bb842d212b3702457486ea4af8bc
LeoRoma <xiajtn@gmail.com>
2021-05-25 10:19:55 +0100

    Loading page animationa added

--- src/home/loading.js
+++ src/home/loading.js
@@ -1,17 +1,28 @@
-const percentage = document.querySelector('.loading-percentage');
-
 function loading(){
+    const percentage = document.querySelector('.loading-percentage');
     let counter = 0;
     const id = setInterval(count, 20);
 
     function count(){
         if(counter === 100){
             clearInterval(id);
+            makeVisible();
+            moveUp()
         }else{
             counter += 1;
             percentage.innerHTML = counter + "%";
         }
     }
+}
+
+
+function makeVisible(){
+    body.style.overflow = "visible";
+}
+
+function moveUp(){
+    const loadingPage = document.querySelector('.loadingpage-container');
+    loadingPage.style.transform = "translateY(-100%)";
 }
 
 loading();
